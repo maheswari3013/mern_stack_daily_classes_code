2ec79a7e6bbb1ba35f5758223eb66c2c5fffa7eb
Maheswari <dmahi3224@gmail.com>
2026-05-07 12:13:53 +0530

    update 12 day code

---
+++ my_react_app/src/App .jsx
@@ -0,0 +1,17 @@
+import react from "react";
+import Nav from "./Nav";
+
+const App=()=>{
+    return (
+      <p>
+        <nav/>
+        <ol>
+            <li>list-1</li>
+            <li>list-2</li>
+            <li>list-3</li>
+        </ol>
+      </p>
+    )
+}
+
+export default App;
---
+++ my_react_app/src/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Nav =()=>{
+    return(
+        <nav>
+            <ol>
+                <li>Home</li>
+                <li>Login</li>
+                <li>Register</li>
+                <li>About</li>
+            </ol>
+        </nav>
+    )
+}
+
+export default Nav;
